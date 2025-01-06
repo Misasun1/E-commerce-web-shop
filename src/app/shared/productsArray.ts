a1aa65f2e7d162cf8db835509a1ecaeb4262ad7a
Misasun1 <misa1996@yahoo.com>
2025-01-06 21:42:41 +0100

    corrections

--- src/app/shared/productsArray.ts
+++ src/app/shared/productsArray.ts
@@ -3,7 +3,7 @@ import { productsModel } from "./productsModel";
 export const products : productsModel [] = [
     {
     name: 'Winsor and Newton - Watercolours',
-    image: '/images/Acrilics.jpg',
+    image: 'images/Acrilics.jpg',
     description: 'Professional watercolours-36 pcs for 5ml',
     descripDetail : 'High-quality artist’s pigments. These fine watercolour paints use high-quality, highly-concentrated pigments made according to proven recipesVibrant colours. With the extraordinary, very high quality watercolour paints you can add even more vibrancy to your artwork',
     price:45.66,
@@ -13,7 +13,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Statement Pillow Case',
-    image: '/images/pillow.jpg',
+    image: 'images/pillow.jpg',
     description: '100% cotton-40x80 cm',
     descripDetail : 'A fun statement, giving a cool vibe to your home.Put a smile to your guest with this item.Additionally a great gift idea for any occasion.The product is handmade with great love in in UK.The quality of our products is very important to us.',
     price:10.99,
@@ -23,7 +23,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Coloured Pink Glasses',
-    image: '/images/tanya-yarosh-ZByCF-zdeRs-unsplash.jpg',
+    image: 'images/tanya-yarosh-ZByCF-zdeRs-unsplash.jpg',
     description: 'set of 2',
     descripDetail : 'High quality glasses made in Italy. Our glasses are made of lead-free crystal glass and guarantee the ideal taste and aroma development thanks to their flat base and generous opening.',
     price:16.74,
@@ -33,7 +33,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Modern Pillow Case',
-    image: '/images/inside-weather-dbH_vy7vICE-unsplash.jpg',
+    image: 'images/inside-weather-dbH_vy7vICE-unsplash.jpg',
     description: 'geometric pattern pink cover- 45x45 cm',
     descripDetail : 'a soft cover, that embellish your bed or sofa, 100% made in cotton',
     price:8.50,
@@ -43,7 +43,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Professional Sketchbook',
-    image: '/images/drawing-notebook.jpg',
+    image: 'images/drawing-notebook.jpg',
     description: '60 pages 240 g/m²',
     descripDetail : 'Drawing pad A4 with thick paper . Suitable for markers, Acrylic paints, Charcoal.The paper is specially designed so that colours do not shine through to the next page, so you can use all types of pens and paints, including alcohol-based and acrylic markers.Every purchase of this sketchbook supports planting new trees and makes it an environmentally friendly choice for artists who care about the environment.',
     price:20.32,
@@ -53,7 +53,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Winsor and Newton-Markers',
-    image: '/images/markers.jpg',
+    image: 'images/markers.jpg',
     description: 'set of 13 markers',
     descripDetail : 'Double tip provides both medium and fine lines.Based on a lightfast pigment ink are sufficient for achieving long-lasting results.',
     price: 35.12,
@@ -63,7 +63,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Ceramic Vase',
-    image: '/images/vase.jpg',
+    image: 'images/vase.jpg',
     description: 'white and light blue vase - 11L x 11W x 19.5H cm',
     descripDetail : 'Handmade ceramic vase: This ceramic vase is made of high-quality handmade ceramic material with thickened base. These ceramic vase are thick and robust, durable and not easy to break, leak-proof and easy to clean. The bottle mouth is smooth and round, the base is non-slip and the centre of gravity is stable, so they do not fall over easily.',
     price:30.00,
@@ -73,7 +73,7 @@ export const products : productsModel [] = [
 
 {
     name: 'White Frame',
-    image: '/images/white-frame.jpg',
+    image: 'images/white-frame.jpg',
     description: 'A4 format-21x30 cm',
     descripDetail : 'A beautiful frame for adorning your memories. Made in MDF wood. ',
     price:12.11,
@@ -83,7 +83,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Faber Castle Pencils',
-    image: '/images/polychromos.jpg',
+    image: 'images/polychromos.jpg',
     description: '120 pcs Coloured Pencils',
     descripDetail : 'Soft and yet stable The latest composition developed by our in-house artists and technicians results in pen cores that allow for a soft, smooth application. They do not crumble even under pressure and hold their pressure point.Highly pigmented, intense, creamy colours for even coverage and effortless application, blending and smudging to create structure and achieve the perfect shade - on paper, canvas, wood and more.',
     price:120.50,
@@ -93,7 +93,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Black Cardboard',
-    image: '/images/black-fondal-paper.jpg',
+    image: 'images/black-fondal-paper.jpg',
     description: 'format A4, color black (10 pcs)',
     descripDetail : 'High quality paper, for rendering beautiful graphic drawings',
     price:10.50,
@@ -103,7 +103,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Succulent Plant',
-    image: '/images/little-plant.jpg',
+    image: 'images/little-plant.jpg',
     description: ' 8cm x 7.5cm ',
     descripDetail : 'a cute artificial plant with included pot-vase, suitable for indoor and outdor. Made of enviromentally friendly PVC. Add a sense of fashion to your room',
     price:4.00,
@@ -113,7 +113,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Wallpapers minimalistic',
-    image: '/images/wallpapers.jpg',
+    image: 'images/wallpapers.jpg',
     description: 'Different assortment of neutral-coloured wallpapers (price for 1 pcs) 9.5 m x 53cm',
     descripDetail : 'High quality materials, enviromentally friendly (made of 100% reusable materials). Easy to mantain it clean, with a damp cotton  cloth  and moderate pressure. Perfect for creating new looks and repairing existing surfaces, durable and non-fading.A simple change can add vitality to your house.Available colors: dark earth, cinnamon,beige eggshell ',
     qtty:1,
@@ -123,7 +123,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Professional Paint Brushes',
-    image: '/images/paint-brushes.jpg',
+    image: 'images/paint-brushes.jpg',
     description: 'set of 12 pcs',
     descripDetail : 'Improving your drawing skills has never been easy,No hair loss thanks to our strong hair technology with special adhesive and extra firm ferrule, real wood handle with unique surface, high-quality synthetic sable hair with very high colour absorption.Our watercolour brush set consists of 8 carefully selected sizes that allow maximum flexibility: from the very fine brush of size 3/0 to the large brush of size 13.',
     price:23.95,
@@ -133,7 +133,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Winsor and Newton Ink',
-    image: '/images/ink.jpg',
+    image: 'images/ink.jpg',
     description: 'Caligraphy Ink Set- price for 1 pcs',
     descripDetail : 'High quality ink Ideal for writing a journal,illustrations,writing letters or for  graphic drawing. It is based with soluble dyes and shellac.The paint dries particularly quickly and is also ideal for mixed techniques ',
     price:3.99,
@@ -143,7 +143,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Plate',
-    image: '/images/plate.jpg',
+    image: 'images/plate.jpg',
     description: '25cm diameter plate. Eco-friendly and BPA free',
     descripDetail : 'A beautiful plate in ceramic. Made in France.Microwave and refrigerator safe.Can be used for its daily purpose or has a decoration',
     price: 15.41,
@@ -154,7 +154,7 @@ export const products : productsModel [] = [
 
 {
     name: 'Candle Burner',
-    image: '/images/candle-burner.jpg',
+    image: 'images/candle-burner.jpg',
     description: 'Enjoy fire-free frangant candle.It is compatible with multiple candles of different heights',
     descripDetail :' It melts the scented candle sent in couple of minute through heat emitted by a 50 watt.The duration of the scent is twice as long as when burning a candle. Ideal for relaxing during yoga, reading, partying, working, sleeping, etc.  Definitely an aesthetic home decoration for living room, bedroom, kitchen, office, etc. ',
     price: 20.50,
